5022525a90c03be866e0d746e1e162effb0f6320
Seth <sethseavers@gmail.com>
2024-02-08 18:48:46 -0600

    Initial skeleton layout with no custom code yet

---
+++ src/main/java/open/seth/springnativediscordbot/SpringNativeDiscordBotApplication.java
@@ -0,0 +1,13 @@
+package open.seth.springnativediscordbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNativeDiscordBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNativeDiscordBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/open/seth/springnativediscordbot/SpringNativeDiscordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package open.seth.springnativediscordbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNativeDiscordBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
