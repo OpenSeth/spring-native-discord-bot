17d9c42313afe0e9b6217473e43909ef4186e6b0
FatCake <sethseavers@gmail.com>
2024-03-06 19:20:40 -0600

    Experimenting with AOT hints

--- src/main/java/open/seth/springnativediscordbot/config/AOTHintsConfig.java
+++ src/main/java/open/seth/springnativediscordbot/config/AOTHintsConfig.java
@@ -1,17 +1,24 @@
 package open.seth.springnativediscordbot.config;
 
 import net.dv8tion.jda.api.entities.Guild;
-import org.springframework.aot.hint.RuntimeHints;
-import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import net.dv8tion.jda.internal.JDAImpl;
+import net.dv8tion.jda.internal.entities.GuildImpl;
+import org.springframework.aot.hint.*;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
-@ImportRuntimeHints(AOTHintsConfig.GraphQlResourcesRegistrar.class)
+@ImportRuntimeHints(AOTHintsConfig.ReflectionHintsRegistrar.class)
 public class AOTHintsConfig {
-    static class GraphQlResourcesRegistrar implements RuntimeHintsRegistrar {
+    static class ReflectionHintsRegistrar implements RuntimeHintsRegistrar {
 
         @Override
         public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-            hints.reflection().registerType(Guild.class);
+            hints.reflection().registerType(GuildImpl.class);
+            hints.reflection().registerType(Guild.class, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+            try {
+                hints.reflection().registerConstructor(GuildImpl.class.getConstructor(JDAImpl.class, int.class), ExecutableMode.INTROSPECT);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
